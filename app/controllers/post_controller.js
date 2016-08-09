f128cf5959e866afe12c2d3386a3f2b37e577434
Ian Bateman <ian.h.bateman.18@dartmouth.edu>
2016-08-09 17:51:07 -0400

    basic homework finished

---
+++ app/controllers/post_controller.js
@@ -0,0 +1,44 @@
+import Post from '../models/post_model';
+
+export const createPost = (req, res) => {
+  // res.send('post should be created here');
+  const post = new Post();
+  post.title = req.body.title;
+  post.tags = req.body.tags;
+  post.content = req.body.content;
+  post.save()
+    .then(result => {
+      res.json({ message: 'Post created!' });
+    })
+    .catch(error => {
+      res.json({ error });
+    });
+};
+export const getPosts = (req, res) => {
+  Post.find()
+  .then(posts => {
+    const cleanPosts = posts.map(post => {
+      return { id: post._id, title: post.title, tags: post.tags, content: post.content };
+    });
+    res.json(cleanPosts);
+  });
+};
+export const getPost = (req, res) => {
+  Post.findById(req.params.id)
+  .then(post => {
+    const cleanPost = { id: post._id, title: post.title, tags: post.tags, content: post.content };
+    res.json(cleanPost);
+  });
+};
+export const deletePost = (req, res) => {
+  Post.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.json('post deleted');
+  });
+};
+export const updatePost = (req, res) => {
+  Post.findOneAndUpdate({ _id: req.params.id }, { title: req.body.title, tags: req.body.tags, content: req.body.content })
+  .then(() => {
+    res.json('post updated');
+  });
+};
